457d19c163fedf780bc2f044b7237256ddd37e02
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-04-02 01:21:15 +0300

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,18 +1,30 @@
 class ArtistsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :find_artist, except: [:index, :new, :create]
 def index
- @artists= Artist.all
+  @artists = current_user.artists.all
+
 end
 def show
-    @artist = Artist.find(params[:id])
+    # @artist = Artist.find(params[:id])
+    if @artist.user != current_user
+      flash[:notice] = 'Not allowed!'
+      redirect_to artists_path
+      # we gite it form rails routes prifixe
+    end
     @songs = @artist.songs
   end
   def new
     @artist = Artist.new
   end
   def create
-    Artist.create(params.require(:artist).permit(:name, :albums, :hometown, :img))
-    redirect_to artists_path
+    @artist = Artist.new(artist_params)
+    @artist.user = current_user
+    if @artist.save
+     redirect_to artists_path
+    else
+      render 'new'
+    end
   end
   def edit
     @artist = Artist.find(params[:id])
@@ -29,3 +41,12 @@ def show
     redirect_to artists_path
   end
 end
+
+private
+  def artist_params
+    params.require(:artist).permit(:name, :albums, :hometown, :img)
+  end
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_202510) do
+ActiveRecord::Schema.define(version: 2020_04_01_213222) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_04_01_202510) do
     t.integer "albums"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_artists_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
